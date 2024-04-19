81ef2e5d1a19d3eb4a10184c351551fedae422f6
samwel141 <samwelwilson141@gmail.com>
2024-04-19 17:49:15 +0300

    initial commits

--- server.js
+++ server.js
@@ -1,51 +1,54 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const morgan = require('morgan')
-const bodyParser = require('body-parser')
+// const express = require('express')
+// const mongoose = require('mongoose')
+// const morgan = require('morgan')
+// const bodyParser = require('body-parser')
 
-const dotenv = require('dotenv')
-dotenv.config()
-
-
-const BloodCenterRoute = require('./routes/BloodCenterRoutes')
-const DonorAuthRoute = require('./routes/DonorRoutes')
+// const dotenv = require('dotenv')
+// dotenv.config()
 
 
-
-mongoose.connect('mongodb://127.0.0.1:27017/tanzania-blood-donors-db')
-const db = mongoose.connection
-db.on('error', (err) => {
-    console.log(err)
-} )
-
-
-db.on('open', () => {
-    console.log("Database connection Established!")
-})
+// const BloodCenterRoute = require('./routes/BloodCenterRoutes')
+// const DonorAuthRoute = require('./routes/DonorRoutes')
 
 
 
+// mongoose.connect('mongodb://127.0.0.1:27017/tanzania-blood-donors-db')
+// const db = mongoose.connection
+// db.on('error', (err) => {
+//     console.log(err)
+// } )
 
 
-const app = express()
-
-app.use(morgan('dev'))
-app.use(bodyParser.urlencoded({extended: true}))
-
-const PORT = process.env.PORT || 3001
-
-app.use((req, res, next) => {
-    res.status(404).send('404: Not Found');
-  });
-app.get('/', (req, res) => {
-    res.send('Welcome to Tanzania Blood Donors app!' + ' ' + 
-              'Use /api/donors to fetch all donors and /api/centers to fetch all blood centers');
-  });
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+// db.on('open', () => {
+//     console.log("Database connection Established!")
+// })
 
 
-app.use('/api/centers', BloodCenterRoute)
-app.use('/api/donors', DonorAuthRoute)
+
+
+
+// const app = express()
+
+// app.use(morgan('dev'))
+// app.use(bodyParser.urlencoded({extended: true}))
+
+// const PORT = process.env.PORT || 3001
+
+// app.use((req, res, next) => {
+//     res.status(404).send('404: Not Found');
+//   });
+// app.get('/', (req, res) => {
+//     res.send('Welcome to Tanzania Blood Donors app!' + ' ' + 
+//               'Use /api/donors to fetch all donors and /api/centers to fetch all blood centers');
+//   });
+
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`)
+// })
+
+
+// app.use('/api/centers', BloodCenterRoute)
+// app.use('/api/donors', DonorAuthRoute)
+
+// export default app;
+
