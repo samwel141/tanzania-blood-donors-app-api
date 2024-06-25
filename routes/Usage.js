075cd926a624912b5a30c4adc9c21588c292fb6e
samwel141 <samwelwilson141@gmail.com>
2024-06-25 09:02:38 +0300

    fix: fix arrangement

--- routes/Usage.js
+++ routes/Usage.js
@@ -5,9 +5,9 @@ const BloodUsage = require('../controllers/BloodUsageController');
 
 
 // router.get('/', authenticateCenter, express.json(), BloodCenterController.allBloodCenters)
+router.get('/donor/:donor_id', BloodUsage.getAllUsageByDonorId)
 router.get('/', express.json(), BloodUsage. getAllUsage)
 router.post('/', express.json(), BloodUsage. postUsage)
-router.get('/donor/:donor_id', BloodUsage.getAllUsageByDonorId)
 router.get('/center/:center_id', BloodUsage.getAllUsageByDonorId)
 
 module.exports = router
